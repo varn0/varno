826845034328e171691c7092fdc419fe2dce5f3d
varn0 <ajanerom@gmail.com>
2026-04-26 20:30:55 +0200

    refactor: split App.css into base/tech/shared with design token system

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/App.css'
+import './styles/base.css'
+import './styles/shared.css'
+import './styles/tech.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
